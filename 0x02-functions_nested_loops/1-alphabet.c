4fbd2942172ecf609b36d112af23acee8bb28458
19dani21 <danielniguse64@gmail.com>
2023-03-22 04:28:07 -0700

    A program that prints lowercase alphabet letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print lowercase alphabet letters
+ * _putchar - return a character
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
