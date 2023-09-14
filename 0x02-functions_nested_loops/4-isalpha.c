ca47fea482298ad40e9a8789b6b362345b1ee8c6
19dani21 <danielniguse64@gmail.com>
2023-09-14 04:02:21 +0100

    correcting betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,8 +4,10 @@
  * main - check the code.
  *
  * Return: Always 0.
+ * _isalpha: checks if a char is alpha.
+ * c: char
  */
-int _isalpha(int c) {
-  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
-
